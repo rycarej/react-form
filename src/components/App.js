7d29b6e9c0ea33d03aa50b029e711eabb42237fe
Vladislav Podkaura <tripledoublehs@ukr.net>
2020-04-03 17:23:35 +0300

    add more fields

--- src/components/App.js
+++ src/components/App.js
@@ -9,14 +9,33 @@ export default class App extends React.Component {
       username: "",
       password: "",
       repeatPassword: "",
-      country: ""
+      country: "",
+      gender: "",
+      agree: true,
+      avatar: ""
     }
   }
 
-  onChange = (event) => {
+  onChange = event => {
     this.setState({
       [event.target.name]: event.target.value
     })
+  }
+
+  onChangeAgree = event => {
+    this.setState({
+      [event.target.name]: event.target.checked
+    })
+  }
+
+  onChangeAvatar = event => {
+    const reader = new FileReader();
+    reader.onload = event => {
+      this.setState({
+        avatar: event.target.result
+      })
+    }
+    reader.readAsDataURL(event.target.files[0]);
   }
 
   onSubmit = (event) => {
@@ -82,6 +101,62 @@ export default class App extends React.Component {
             >
               {this.getOptionsItems(countries)}
             </select>
+          </div>
+          <fieldset className="form-group">
+            <div>Gender</div>
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="radio"
+                id="male"
+                name="gender"
+                value="male"
+                checked={this.state.gender === "male"}
+                onChange={this.onChange}
+              />
+              <label className="form-check-label" htmlFor="male">
+                Male
+              </label>
+            </div>
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="radio"
+                id="female"
+                name="gender"
+                value="female"
+                checked={this.state.gender === "female"}
+                onChange={this.onChange}
+              />
+              <label className="form-check-label" htmlFor="female">
+                Female
+              </label>
+            </div>
+          </fieldset>
+          <div className="form-group">
+            <label htmlFor="avatar">Avatar</label>
+            <input
+              type="file"
+              className="form-control-file"
+              placeholder="Enter username"
+              id="avatar"
+              name="avatar"
+              onChange={this.onChangeAvatar}
+            />
+          </div>
+          <div className="form-check mb-2">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              id="agree"
+              name="agree"
+              value={this.state.agree}
+              onChange={this.onChangeAgree}
+              checked={this.state.agree}
+            />
+            <label className="form-check-label" htmlFor="agree">
+              Confirm the processing of data
+            </label>
           </div>
           <button type="submit" className="btn btn-primary w-100" onClick={this.onSubmit}>
             Submit
